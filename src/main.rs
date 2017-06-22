d1f15b715f1cefd76ac910f6bef7ebdfc6473ab7
quininer kel <quininer@live.com>
2017-06-22 23:31:48 +0800

    init project

---
+++ src/main.rs
@@ -0,0 +1,79 @@
+#[macro_use] extern crate structopt_derive;
+extern crate structopt;
+
+extern crate cargo;
+extern crate semver;
+extern crate tabwriter;
+
+mod query;
+
+use std::path::Path;
+use std::borrow::Cow;
+use std::io::{ self, Write };
+use cargo::ops;
+use cargo::core::Workspace;
+use cargo::core::registry::PackageRegistry;
+use cargo::util::Config as CargoConfig;
+use cargo::util::errors::CargoResult;
+use cargo::util::important_paths::find_root_manifest_for_wd;
+use tabwriter::TabWriter;
+use structopt::StructOpt;
+use query::query_latest;
+
+
+#[derive(StructOpt)]
+#[structopt]
+pub struct Config {
+    /// manifest path
+    #[structopt(short = "m", long = "manifest")]
+    manifest: Option<String>
+}
+
+
+#[inline]
+fn start(config: Config) -> CargoResult<()> {
+    const EMPTY_VERSION: Cow<'static, str> = Cow::Borrowed("--");
+
+    let cargo_config = CargoConfig::default()?;
+    let workspace = if let Some(ref manifest) = config.manifest {
+        Workspace::new(&Path::new(manifest).canonicalize()?, &cargo_config)?
+    } else {
+        let root = find_root_manifest_for_wd(config.manifest, cargo_config.cwd())?;
+        Workspace::new(&root, &cargo_config)?
+    };
+    let mut registry = PackageRegistry::new(&cargo_config)?;
+    let (_, resolve) = ops::resolve_ws(&workspace)?;
+
+    let mut tw = TabWriter::new(vec!());
+
+    for package in resolve.iter() {
+        let (compat_latest, latest) = query_latest(&mut registry, &package)?;
+        if compat_latest.is_none() && latest.is_none() {
+            continue
+        }
+
+        writeln!(&mut tw, "{}:\t{}\t{}\t{}",
+            package.name(),
+            package.version(),
+            compat_latest
+                .map(|s| s.version().to_string())
+                .map(Cow::Owned)
+                .unwrap_or(EMPTY_VERSION),
+            latest
+                .map(|s| s.version().to_string())
+                .map(Cow::Owned)
+                .unwrap_or(EMPTY_VERSION)
+        )?;
+    }
+
+    io::stdout().write_all(&tw.into_inner()
+        .map_err(|err| io::Error::new(err.error().kind(), err.to_string()))?
+    )?;
+
+    Ok(())
+}
+
+fn main() {
+    let config = Config::from_args();
+    start(config).unwrap();
+}
---
+++ src/query.rs
@@ -0,0 +1,32 @@
+use cargo::core::registry::{ PackageRegistry, Registry };
+use cargo::core::dependency::Dependency;
+use cargo::core::package_id::PackageId;
+use cargo::core::summary::Summary;
+use cargo::util::errors::CargoResult;
+use semver::VersionReq;
+
+
+pub fn query_latest(registry: &mut PackageRegistry, package: &PackageId)
+    -> CargoResult<(Option<Summary>, Option<Summary>)>
+{
+    let dep = Dependency::new_override(package.name(), package.source_id());
+    let results = registry.query(&dep)?;
+
+    let latest = results.iter()
+        .max_by_key(|summary| summary.version())
+        .and_then(|summary|
+            if summary.version() > package.version() { Some(summary) }
+            else { None }
+        )
+        .cloned();
+    let compatible_latest = results.iter()
+        .filter(|summary| VersionReq::exact(package.version()).matches(summary.version()))
+        .max_by_key(|summary| summary.version())
+        .and_then(|summary|
+            if summary.version() > package.version() { Some(summary) }
+            else { None }
+        )
+        .cloned();
+
+    Ok((compatible_latest, latest))
+}
